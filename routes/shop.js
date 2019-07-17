e308254442b79407b12714dd982d3e355cd1d0cd
David Yu <davidpyu@gmail.com>
2019-07-17 15:10:24 -0500

    Restrict invoice access to owner

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // Controller Imports
 const shopController = require('../controllers/shop');
 
+// Middleware
+const isAuth = require('../middleware/is-auth');
+
 router.get("/", shopController.getIndex);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
@@ -12,6 +15,7 @@ router.post('/create-order', shopController.postOrder);
 // router.get('/checkout', shopController.getCheckout);
 router.get('/products', shopController.getProducts);
 router.get('/product/:productId', shopController.getProduct);
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
 module.exports = router;
